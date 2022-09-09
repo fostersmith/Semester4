e9f35a387d168bafdb02359c537e49afd8f72a78
Foster Smith <fostersmith406@gmail.com>
2022-09-09 09:11:44 -0600

    did work

--- copypackage.bat
+++ copypackage.bat
@@ -1,8 +1,6 @@
 %echo off
-echo %1
-echo %2
 set p=%1\src\%2\*.java
-echo %p%
+echo copying items matching %p% to output.txt
 copy /y nul > output.txt
 for %%y in (%p%) do (
 	for /f "delims=" %%l in (%%y) do echo(%%l >> output.txt
