5f856b9c086494e4dcce5b1bb6e5a11ad4432019
Foster Smith <fostersmith406@gmail.com>
2022-09-21 12:04:03 -0600

    did work

--- Chapter13/src/carlysCatering/DisplayDinnerEventFile.java
+++ Chapter13/src/carlysCatering/DisplayDinnerEventFile.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -19,19 +20,44 @@ import javax.swing.JPanel;
 public class DisplayDinnerEventFile extends JFrame implements ActionListener {
 	
 	ArrayList<String[]> recs;
-	JPanel displayPanel;
+	int onRec = 0;
+	JPanel displayPanel, buttonPanel;
 	JLabel eventNumLabel, eventTypeLabel, guestsLabel, priceLabel;
-	JButton nextButton, backButton, closeButton;
+	JButton nextButton, backButton;
+	boolean buttonDown = false;
 	static DisplayDinnerEventFile f1;
 	
 	private DisplayDinnerEventFile(ArrayList<String[]> recs) {
-		this.recs = recs;
 		
 		eventNumLabel = new JLabel();
 		eventTypeLabel  = new JLabel();
 		guestsLabel = new JLabel();
 		priceLabel = new JLabel();
+		
+		nextButton = new JButton("Next");
+		backButton = new JButton("Back");
+		
+		nextButton.addActionListener(this);
+		backButton.addActionListener(this);
+		
 		displayPanel = new JPanel(new GridLayout(5,1));
+		buttonPanel = new JPanel(new GridLayout(1,2));
+		
+		displayPanel.add(eventNumLabel);
+		displayPanel.add(eventTypeLabel);
+		displayPanel.add(guestsLabel);
+		displayPanel.add(priceLabel);
+		
+		buttonPanel.add(backButton);
+		buttonPanel.add(nextButton);
+		
+		displayPanel.add(buttonPanel);
+		
+		this.add(displayPanel);
+		this.setBounds(0,0,200,500);
+		
+		this.recs = recs;
+		setRec(0);
 	}
 	
 	public static DisplayDinnerEventFile readFromFile(Path file) throws IOException {
@@ -48,15 +74,36 @@ public class DisplayDinnerEventFile extends JFrame implements ActionListener {
 		return new DisplayDinnerEventFile(recs);
 	}
 		
+	public void setLabels(String[] rec) {
+		eventNumLabel.setText("Event Number: "+rec[0]);
+		eventTypeLabel.setText("Event Type: "+rec[1]);
+		guestsLabel.setText("Guests: "+rec[2]);
+		priceLabel.setText("Price: $"+rec[3]);
+	}
+	
+	public void setRec(int onRec) {
+		setLabels(recs.get(onRec));
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == nextButton) {
-			
+			onRec = (onRec+1)%recs.size();
+			setRec(onRec);
 		} else if (e.getSource() == backButton) {
-			
-		} else if (e.getSource() == closeButton) {
-			
+			onRec = (onRec+(recs.size()-1))%recs.size();
+			setRec(onRec);
 		}
+		System.out.println("On Record: "+onRec);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		Path file = Paths.get("events.txt");
+		f1 = DisplayDinnerEventFile.readFromFile(file);
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f1.setVisible(true);
+		
 	}
 	
 }
