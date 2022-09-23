a2d62f6a375d072e886ab52f9488fac0db94cc0f
fostersmith <fostersmith406@gmail.com>
2022-09-23 08:20:19 -0600

    did work

--- DuckGame/src/client/DuckGame.java
+++ DuckGame/src/client/DuckGame.java
@@ -1,12 +1,17 @@
 package client;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -16,9 +21,10 @@ public class DuckGame extends JPanel {
 
 	ArrayList<Duck> ducks;
 	BufferedImage canvas;
+	DuckGameConfig config;
 	
 	public DuckGame(DuckGameConfig f) {
-		
+		canvas = new BufferedImage(500,500,BufferedImage.TYPE_INT_ARGB);
 	}
 	
 	@Override
@@ -29,8 +35,13 @@ public class DuckGame extends JPanel {
 		g2d.drawImage(canvas, 0, 0, canvas.getWidth(), canvas.getHeight(), null);
 	}
 	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(canvas.getWidth(), canvas.getHeight());
+	}
+	
 	public void render() {
-		
+
 	}
 	
 	public void doTick() {
@@ -38,11 +49,53 @@ public class DuckGame extends JPanel {
 	}
 	
 	public static void main(String[] args) {
-		String filepath = JOptionPane.showInputDialog("Enter the Path of the Configuration File");
-		Path configfile = Paths.get(filepath);
-		int password = Integer.parseInt(JOptionPane.showInputDialog("Enter Password"));
-		DuckGameConfig config = DuckGameConfig.readFromFile(configfile, password);
-		DuckGame f1 = new DuckGame(config);
+		DuckGameConfig config = null;
+		boolean cancel = false;
+		while(config == null && !cancel) {
+			String filepath = JOptionPane.showInputDialog("Enter the Path of the Configuration File");
+			if(filepath != null) {
+				filepath += ".dgc";
+				Path configfile = Paths.get(filepath);
+				if(Files.exists(configfile)) {
+					String username = JOptionPane.showInputDialog("Enter Username");
+					int password = Integer.parseInt(JOptionPane.showInputDialog("Enter Password"));
+					try {
+						
+						config = DuckGameConfig.readFromFile(configfile, password);
+						if(!config.username.equals(username)) {
+							JOptionPane.showMessageDialog(null, "Username or password was incorrect", "Login Error", JOptionPane.ERROR_MESSAGE);
+							config = null;
+						}
+						
+					} catch(IOException e) {
+						
+						JOptionPane.showMessageDialog(null, "An IO Error Occurred\n"+e, "Error", JOptionPane.ERROR_MESSAGE);
+						
+					}
+				} else {
+					int opt = JOptionPane.showOptionDialog(null, "That file does not exist. Create a new one?", "No File Exists", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+					if(opt == JOptionPane.YES_OPTION) {
+						String username = JOptionPane.showInputDialog("Enter Username");
+						int password = Integer.parseInt(JOptionPane.showInputDialog("Enter Password"));
+						try {
+							config = DuckGameConfig.createBlankFile(configfile, username, password);
+						} catch (IOException e1) {
+							JOptionPane.showMessageDialog(null, "An IO Error Occurred\n"+e1, "Error", JOptionPane.ERROR_MESSAGE);
+						}
+					}
+				}
+			} else
+				cancel = true;
+		}
+		if(!cancel) {
+			DuckGame game = new DuckGame(config);
+			JFrame f1 = new JFrame();
+			f1.add(game);
+			game.render();
+			f1.pack();
+			f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			f1.setVisible(true);
+		}
 	}
 	
 }
--- DuckGame/src/client/DuckGameConfig.java
+++ DuckGame/src/client/DuckGameConfig.java
@@ -2,34 +2,75 @@ package client;
 
 import static java.nio.file.StandardOpenOption.READ;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import lib.Encryption;
 
 public class DuckGameConfig extends Encryption {
 
+	private final static Path DEFAULT_BACKGROUND = Paths.get("blank.png");
+	String username;
 	Path background;
 	ArrayList<Path> ducks;
+	
+	private DuckGameConfig(Path background) {
+		this.background = background;
+		ducks = new ArrayList<>();
+	}
 	
 	private DuckGameConfig() {
 		ducks = new ArrayList<>();
 	}
 	
 	public static DuckGameConfig readFromFile(Path file, int password) throws IOException {
+		DuckGameConfig conf = new DuckGameConfig();
 		InputStream input = Files.newInputStream(file, READ);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		String username = decrypt(reader.readLine(), password);
+		conf.username = username;
 		String bgpath = decrypt(reader.readLine(), password);
-		return null;
+		conf.background = Paths.get(bgpath);
+		String s;
+		while((s = reader.readLine())!=null) {
+			String duckPath = decrypt(s, password);
+			conf.ducks.add(Paths.get(duckPath));
+		}
+		return conf;
 	}
 	
-	public static void writeToFile(DuckGameConfig config, Path file, int password) {
-		
+	public static void writeToFile(DuckGameConfig config, Path file, int password) throws IOException {
+		OutputStream output = new BufferedOutputStream(Files.newOutputStream(file));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output));
+		String s = encrypt(config.username, password);
+		writer.write(s, 0, s.length());
+		writer.newLine();
+		s = encrypt(config.background.toString(), password);
+		writer.write(s, 0, s.length());
+		for(Path p:config.ducks) {
+			writer.newLine();
+			s = encrypt(p.toString(), password);
+			writer.write(s, 0, s.length());
+		}
+		writer.flush();
+		output.close();
+	}
+
+	public static DuckGameConfig createBlankFile(Path file, String username, int password) throws IOException {
+		DuckGameConfig con = new DuckGameConfig(DEFAULT_BACKGROUND);
+		con.username = username;
+		writeToFile(con, file, password);
+		return con;
 	}
 	
 }
