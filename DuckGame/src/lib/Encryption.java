fcd4a03aac04e299b5932a97d5efaf87584596fd
Foster Smith <fostersmith406@gmail.com>
2022-10-03 11:37:08 -0600

    automated commit

--- DuckGame/src/lib/Encryption.java
+++ DuckGame/src/lib/Encryption.java
@@ -5,7 +5,7 @@ public abstract class Encryption {
 	public static String encrypt(String plaintext, int password) {
 		StringBuilder ciphertext = new StringBuilder();
 		for(int c:plaintext.chars().toArray()) {
-			ciphertext.append((char)(c+password));
+			ciphertext.append((char)((c+password%65537)));
 		}
 		return ciphertext.toString();
 	}
@@ -13,7 +13,7 @@ public abstract class Encryption {
 	public static String decrypt(String ciphertext, int password) {
 		StringBuilder plaintext = new StringBuilder();
 		for(int c:ciphertext.chars().toArray()) {
-			plaintext.append((char)(c-password));
+			plaintext.append((char)((c-password)%65537));
 		}
 		return plaintext.toString();		
 	}
