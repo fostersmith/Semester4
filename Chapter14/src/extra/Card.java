de18ae35dd24c72faf29c64de7f214260fd5068e
Foster Smith <fostersmith406@gmail.com>
2022-10-27 09:11:50 -0600

    automated commit

--- Chapter14/src/extra/Card.java
+++ Chapter14/src/extra/Card.java
@@ -1,9 +1,23 @@
 package extra;
 
-public class Card {
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JButton;
+
+public class Card extends JButton {
+	
+	public static final int PX_WIDTH = 40, PX_HEIGHT = 80;
+	
 	public static final int HEARTS = 14, DIAMONDS = 15, CLUBS = 16, SPADES = 17;
 	public static final int ACE = 1, JACK = 11, QUEEN = 12, KING = 13;
+	public static final int FACEDOWN = 0, FACEUP = 1, HIGHLIGHTED = 2;
+	
 	private final int suit, value;
+	private BufferedImage img;
+	private int state;
+	
 	public Card(int suit, int value) {
 		if(!(suit == HEARTS || suit == DIAMONDS || suit == CLUBS || suit == SPADES))
 			throw new IllegalArgumentException("Invalid Suit");
@@ -17,5 +31,32 @@ public class Card {
 	}
 	public int getValue() {
 		return value;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		if(!(state==FACEDOWN || state ==FACEUP || state == HIGHLIGHTED))
+			throw new IllegalArgumentException("Invalid state");
+		this.state = state;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(img, 0, 0, PX_WIDTH, PX_HEIGHT, null);
+		//TODO highlighting
+	}
+	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(PX_WIDTH, PX_HEIGHT);
+	}
+	
+	public static BufferedImage getImage(int suit, int value) {
+		if(!(suit == HEARTS || suit == DIAMONDS || suit == CLUBS || suit == SPADES))
+			throw new IllegalArgumentException("Invalid Suit");
+		if(value < ACE || value > KING)
+			throw new IllegalArgumentException("Invalid Value");
+		return null; //TODO
 	}
 }
--- Chapter14/src/extra/TableauPile.java
+++ Chapter14/src/extra/TableauPile.java
@@ -1,10 +1,37 @@
 package extra;
 
-import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.util.ArrayList;
 
-public class TableauPile extends Canvas {
+import javax.swing.JPanel;
+
+public class TableauPile extends JPanel {
 	
+	public static final int PX_VISIBLE_SPACE = 10; //vertical distance between cards
 	ArrayList<Card> faceDown, faceUp;
+	
+	public TableauPile() {
+		super();
+		setLayout(null);
+		faceDown = new ArrayList<>();
+		faceUp = new ArrayList<>();
+	}
+	
+	public void faceUpAdd(Card c) {
+		faceUp.add(c);
+		add(c);
+		c.setBounds(0,(faceDown.size()+faceUp.size())*PX_VISIBLE_SPACE,Card.PX_WIDTH,Card.PX_HEIGHT);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(Card.PX_WIDTH, (faceDown.size()+faceUp.size()-1)*PX_VISIBLE_SPACE+Card.PX_HEIGHT);
+	}
 	
 }
