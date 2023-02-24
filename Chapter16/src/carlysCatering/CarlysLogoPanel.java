d852a9205649d8cfbb0ebf9c26fb65456d61b7d1
Foster Smith <fostersmith406@gmail.com>
2023-02-24 12:00:55 -0700

    automated commit

--- Chapter16/src/carlysCatering/CarlysLogoPanel.java
+++ Chapter16/src/carlysCatering/CarlysLogoPanel.java
@@ -1,5 +1,6 @@
 package carlysCatering;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -24,12 +25,39 @@ public class CarlysLogoPanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		int sandwichHeight = height/2;
+		int sandwichStart = (height-sandwichHeight)/2;
+		
+		int breadHeight = height/16;
+		int meatHeight = (sandwichHeight-2*breadHeight)/4;
+		int tomatoHeight = (sandwichHeight-2*breadHeight)/4;
+		int lettuceHeight = (sandwichHeight-2*breadHeight)/8; //amount lettuce rises above tomato
+		int cheeseHeight = 3*(sandwichHeight-2*breadHeight)/8; //amount cheese rises above top of lettuce
+		
 		Graphics2D g2d = (Graphics2D)g;
-		g2d.drawRect(width/4, height/4, width/2, height/16);
-		g2d.drawPolygon(new int[] {width/4, width/2, (3*width)/4}, new int[] {height/4+height/16, height/4+height/8, height/4+height/16}, 3);
-		for(int i = 0; i < 11; ++i) {
-			g2d.drawOval(width/4+(i*width/22), height/, i, i)
+		g2d.setColor(Color.orange);
+		g2d.fillRect(width/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight+cheeseHeight+meatHeight, width/2, breadHeight);
+		
+		g2d.setColor(Color.PINK);
+		g2d.fillRect(width/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight+tomatoHeight, width/2, meatHeight);
+		
+		g2d.setColor(Color.RED);
+		g2d.fillRect(width/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight, width/2, tomatoHeight);
+		
+		g2d.setColor(Color.GREEN);
+		int lettuceDiameter = 2*lettuceHeight;
+		int numLettuces = (width/2)/(lettuceDiameter);
+		g2d.fillRect(width/4+width/(4*numLettuces), sandwichStart+breadHeight+cheeseHeight, width/2-width/(2*numLettuces), width/(4*numLettuces-4));
+		for(int i = 0; i < numLettuces; ++i) {
+			g2d.fillOval(width/4+(i*(width/2)/numLettuces), sandwichStart+breadHeight+cheeseHeight, lettuceDiameter, lettuceDiameter);
 		}
+		
+		g2d.setColor(Color.yellow);
+		g2d.fillPolygon(new int[] {width/4, width/2, (3*width)/4}, new int[] {sandwichStart + breadHeight, sandwichStart + breadHeight + sandwichHeight/4, sandwichStart + breadHeight}, 3);
+		
+		g2d.setColor(Color.ORANGE);
+		g2d.fillRect(width/4, sandwichStart, width/2, breadHeight);
 	}
 	
 	//temp
