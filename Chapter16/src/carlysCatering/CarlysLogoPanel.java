5385c8d2051c117d1f9930568fa4f4304b6536db
Foster Smith <fostersmith406@gmail.com>
2023-02-24 23:18:09 -0700

    automated commit

--- Chapter16/src/carlysCatering/CarlysLogoPanel.java
+++ Chapter16/src/carlysCatering/CarlysLogoPanel.java
@@ -2,8 +2,11 @@ package carlysCatering;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -26,18 +29,20 @@ public class CarlysLogoPanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		int sandwichHeight = height/2;
+		int sandwichHeight = height/3;
 		int sandwichStart = (height-sandwichHeight)/2;
 		
-		int breadHeight = height/16;
-		int meatHeight = (sandwichHeight-2*breadHeight)/4;
-		int tomatoHeight = (sandwichHeight-2*breadHeight)/4;
-		int lettuceHeight = (sandwichHeight-2*breadHeight)/8; //amount lettuce rises above tomato
-		int cheeseHeight = 3*(sandwichHeight-2*breadHeight)/8; //amount cheese rises above top of lettuce
+		int breadHeight = sandwichHeight/4;
+		int meatHeight = (sandwichHeight)/6;
+		int tomatoHeight = (sandwichHeight)/6; // 2/4+2/6 = 1/2 + 1/3 = 3/6+2/6 = 5/6
+		int lettuceHeight = 1*(sandwichHeight)/12; //amount lettuce rises above tomato
+		int cheeseHeight = 1*(sandwichHeight)/12; //amount cheese rises above top of lettuce
 		
 		Graphics2D g2d = (Graphics2D)g;
 		g2d.setColor(Color.orange);
-		g2d.fillRect(width/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight+cheeseHeight+meatHeight, width/2, breadHeight);
+		g2d.fillOval(width/4-breadHeight/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight+tomatoHeight+meatHeight, breadHeight/2, breadHeight);
+		g2d.fillOval(3*width/4-breadHeight/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight+tomatoHeight+meatHeight, breadHeight/2, breadHeight);
+		g2d.fillRect(width/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight+tomatoHeight+meatHeight, width/2, breadHeight);
 		
 		g2d.setColor(Color.PINK);
 		g2d.fillRect(width/4, sandwichStart+breadHeight+cheeseHeight+lettuceHeight+tomatoHeight, width/2, meatHeight);
@@ -57,7 +62,21 @@ public class CarlysLogoPanel extends JPanel {
 		g2d.fillPolygon(new int[] {width/4, width/2, (3*width)/4}, new int[] {sandwichStart + breadHeight, sandwichStart + breadHeight + sandwichHeight/4, sandwichStart + breadHeight}, 3);
 		
 		g2d.setColor(Color.ORANGE);
+		g2d.fillOval(width/4-breadHeight/4, sandwichStart, breadHeight/2, breadHeight);
+		g2d.fillOval(3*width/4-breadHeight/4, sandwichStart, breadHeight/2, breadHeight);
 		g2d.fillRect(width/4, sandwichStart, width/2, breadHeight);
+		
+		Font logoFont = new Font("Broadway", Font.PLAIN, 100);
+		g2d.setColor(Color.BLUE);
+		g2d.setFont(logoFont);
+				
+		Rectangle2D carlys = logoFont.getStringBounds("Carly's", g2d.getFontRenderContext());
+		Rectangle2D catering = logoFont.getStringBounds("Catering", g2d.getFontRenderContext());
+				
+		g2d.setPaint(new GradientPaint( 0, height/7 +(int)carlys.getCenterY(), Color.BLUE,  0, height/7 - 3*(int)carlys.getCenterY(),Color.WHITE, true));
+		g2d.drawString("Carly's", width/2 - (int)carlys.getCenterX(), height/7 -(int)carlys.getCenterY());
+		g2d.setPaint(new GradientPaint( 0, 6*height/7 + (int)catering.getCenterY(), Color.BLUE,  0, 6*height/7 - 3*(int)catering.getCenterY(),Color.WHITE, true));
+		g2d.drawString("Catering", width/2 - (int)catering.getCenterX(), 6*height/7 - (int)catering.getCenterY());
 	}
 	
 	//temp
