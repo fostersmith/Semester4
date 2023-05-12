3f06af2a3c44ef4817eba2949ed107e7899636ef
Foster Smith <fostersmith406@gmail.com>
2023-05-12 09:12:53 -0600

    automated commit

--- FX/FXPhotoFrame/src/main/java/main/PrimaryController.java
+++ FX/FXPhotoFrame/src/main/java/main/PrimaryController.java
@@ -1,12 +1,36 @@
 package main;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    ToggleButton subOneP, subMultP, subPandP;
+    
+    @FXML
+    ToggleButton locStudio, locOnSite;
+    
+    @FXML
+    Label priceLabel;
+    
+    @FXML
+    public void calcPrice(){
+        int price = 0;
+        
+        if(subOneP.isSelected()){
+            price = 40;
+        } else if (subMultP.isSelected()){
+            price = 75;
+        } else if(subPandP.isSelected()){
+            price = 95;
+        }
+        
+        if(locOnSite.isSelected()){
+            price += 90;
+        }
+        
+        priceLabel.setText("Price: $"+price);
     }
 }
